ba54d26655789737f91c609b83c102d5a0faffb0
Jorge Moreno <jmoreno@paradigmatecnologico.com>
2014-07-20 14:48:14 +0200

    Basic template to build a Django webapp inside Vagrant

--- provision.sh
+++ provision.sh
@@ -8,7 +8,7 @@ fi
 
 # Install basic packages
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential mysql-server python-dev python-pip python-virtualenv libmysqlclient-dev libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev zlib1g-dev
+DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential python-dev python-pip python-virtualenv libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev zlib1g-dev
 
 # Upgrade python packages downloaded from repository
 pip install --upgrade setuptools
@@ -34,11 +34,6 @@ if [ ! -f "/home/vagrant/.bashrc_updated" ]; then
 	cp /home/vagrant/.bashrc /home/vagrant/.bashrc_updated
 fi
 
-# Necessary to access MySQL from outside Vagrant
-sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" | mysql -uroot
-service mysql restart
-
 # Setup virtualenv
 if [ ! -d '/vagrant/project' ]; then
 	mkproject project
@@ -53,7 +48,6 @@ if [ ! -f '/vagrant/project/requirements.txt' ]; then
 
 	echo "
 	Pillow
-	MySQL-python
 	" >> /vagrant/project/requirements/base.txt
 
 	pip install -r /vagrant/project/requirements/local.txt
