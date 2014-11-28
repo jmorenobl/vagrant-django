71809f73d375208ec1c92ba07c0ce558b52ac729
Jorge Moreno Blázquez <jorge.moreno@mixin.es>
2014-11-28 20:23:15 +0100

    Añadida configuración de django para postgresql

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ vagrant-django
 ==============
 
 Bootstrap your Django project with Vagrant
+
+
+Creating a PostgreSQL Database
+==============================
+
+$ sudo su - postgres
+postgres$ createdb dbname -T template_postgis
+postgres$ createuser -P
+Enter name of role to add: dbuser
+Enter password for new role:
+Enter it again:
+Shall the new role be a superuser? (y/n) n
+Shall the new role be allowed to create databases? (y/n) n
+Shall the new role be allowed to create more new roles? (y/n) n
+
+postgres$ psql -c "GRANT ALL PRIVILEGES ON DATABASE dbname TO dbuser;"
--- provision.sh
+++ provision.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+PROJECT_FOLDER=/vagrant/project
+
 if [ -z $1 ]; then
   PROJECT_NAME="myproject"
 else
@@ -8,53 +10,45 @@ fi
 
 # Install basic packages
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential mysql-server python-dev python-pip python-virtualenv libmysqlclient-dev libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev zlib1g-dev
+DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential binutils python-dev python-pip \
+libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev zlib1g-dev gettext postgresql \
+postgresql-contrib libpq-dev libproj-dev gdal-bin libgeoip1 python-gdal postgis postgresql-8.4-postgis
 
 # Upgrade python packages downloaded from repository
 pip install --upgrade setuptools
 pip install --upgrade pip
-pip install --upgrade virtualenv
 
-# Install basic python packages
-pip install virtualenvwrapper 
-
-export WORKON_HOME=/home/vagrant/.virtualenvs
-export PROJECT_HOME=/vagrant
-source /usr/local/bin/virtualenvwrapper.sh
-
-if [ ! -f "/home/vagrant/.bashrc_updated" ]; then
-	echo "
-	export WORKON_HOME=/home/vagrant/.virtualenvs
-	export PROJECT_HOME=/vagrant
-	source /usr/local/bin/virtualenvwrapper.sh
-
-	workon project
-	" >> /home/vagrant/.bashrc
-
-	cp /home/vagrant/.bashrc /home/vagrant/.bashrc_updated
+# Setup folder
+if [ ! -d $PROJECT_FOLDER ]; then
+	mkdir $PROJECT_FOLDER
 fi
 
-# Necessary to access MySQL from outside Vagrant
-sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;" | mysql -uroot
-service mysql restart
+if [ ! -f $PROJECT_FOLDER/requirements.txt ]; then
+	pip install django==1.6.8
 
-# Setup virtualenv
-if [ ! -d '/vagrant/project' ]; then
-	mkproject project
-else
-	workon project
-fi
-
-if [ ! -f '/vagrant/project/requirements.txt' ]; then
-	pip install django
-
-	django-admin.py startproject --template=https://github.com/jmorenobl/django-template-project/archive/master.zip --extension=py,rst,html $PROJECT_NAME /vagrant/project
+	django-admin.py startproject --template=https://github.com/jmorenobl/django-template-project/archive/master.zip --extension=py,rst,html $PROJECT_NAME $PROJECT_FOLDER
 
 	echo "
 	Pillow
-	MySQL-python
-	" >> /vagrant/project/requirements/base.txt
+  psycopg2
+	" >> $PROJECT_FOLDER/requirements/base.txt
 
-	pip install -r /vagrant/project/requirements/local.txt
+	pip install -r $PROJECT_FOLDER/requirements/local.txt
 fi
+
+# Necessary to access PostgreSQL from outside Vagrant
+sed -i "s/^#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/8.4/main/postgresql.conf
+sed -i "s/^local   all         all                               ident/local   all         all                               md5/" /etc/postgresql/8.4/main/pg_hba.conf
+service postgresql-8.4 restart
+
+POSTGIS_SQL_PATH=`pg_config --sharedir`/contrib
+
+su - postgres -c "createdb -E UTF8 template_postgis"
+su - postgres -c "createlang -d template_postgis plpgsql"
+su - postgres -c "psql -d postgres -c \"UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';\""
+su - postgres -c "psql -d template_postgis -f $POSTGIS_SQL_PATH/postgis.sql"
+su - postgres -c "psql -d template_postgis -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql"
+su - postgres -c "psql -d template_postgis -c \"GRANT ALL ON geometry_columns TO PUBLIC;\""
+su - postgres -c "psql -d template_postgis -c \"GRANT ALL ON spatial_ref_sys TO PUBLIC;\""
+
+chown -R vagrant:vagrant /home/vagrant/
